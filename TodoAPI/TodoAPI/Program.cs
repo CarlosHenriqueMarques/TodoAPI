08efdb891426e7f67c2d555c2bdb5da4031bc9c7
Carlos Maia <carlos.marques89@gmail.com>
2019-09-02 17:24:19 -0400

    Criação do projeto

---
+++ TodoAPI/TodoAPI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TodoAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
