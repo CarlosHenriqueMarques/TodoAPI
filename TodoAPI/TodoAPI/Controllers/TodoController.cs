1d8d59bff2f830e643a05c3a305f2da313ec1bdb
Carlos Maia <carlos.marques89@gmail.com>
2019-09-03 11:22:51 -0400

    Adicionand o método delete

--- TodoAPI/TodoAPI/Controllers/TodoController.cs
+++ TodoAPI/TodoAPI/Controllers/TodoController.cs
@@ -73,5 +73,21 @@ namespace TodoAPI.Controllers
             return NoContent();
         }
 
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTodoItem(long id)
+        {
+            var todoItem = await _context.TodoItems.FindAsync(id);
+
+            if (todoItem == null)
+            {
+                return NotFound();
+            }
+
+            _context.TodoItems.Remove(todoItem);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
     }
 }
